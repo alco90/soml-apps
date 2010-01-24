13b9209e8068365131656134f0d542437b8aabe4
Jolyon White <jolyon.white@gmail.com>
2010-01-25 02:15:56 +1100

    Clean up wlanconfig_oml2.
    - Fix some #includes.
    - Fix file header comment.
    - Fix weird, inconsistent indentation and formatting.
    - Clean up some other cruft.

--- wlanconfig/wlanconfig_oml.c
+++ wlanconfig/wlanconfig_oml.c
@@ -1,9 +1,8 @@
 /*
- *  C Implementation: app_sigar
+ *  C Implementation: wlanconfig_oml2
  *
- * Description:  An application based on the libsigar that communicates with 
- *               the oml library
- *
+ * Description:  An application to take measurements from wlanconfig using
+ *               OML.
  *
  * Author: Guillaume Jourjon <guillaume.jourjon@nicta.com.au>, (C) 2008
  *
@@ -36,194 +35,141 @@
 #include <pcap.h>
 #include <errno.h>
 #include <unistd.h>
-#include "ocomm/o_log.h"
-#include "oml2/omlc.h"
-#include "oml2/oml_filter.h"
+#include <ocomm/o_log.h>
+#include <oml2/omlc.h>
 
-
-static OmlMPDef oml_def[] = {  
-{"macAddress", OML_STRING_PTR_VALUE},
-{"RSSI", OML_LONG_VALUE},
-{"DBM", OML_LONG_VALUE}, 
-{"myMacAddress", OML_STRING_PTR_VALUE}, 
-{NULL, (OmlValueT)0},
+static OmlMPDef oml_def[] = {
+  {"macAddress", OML_STRING_PTR_VALUE},
+  {"RSSI", OML_LONG_VALUE},
+  {"DBM", OML_LONG_VALUE},
+  {"myMacAddress", OML_STRING_PTR_VALUE},
+  {NULL, (OmlValueT)0},
 };
 static OmlMP* oml_mp = NULL;
 
-
-
 int
-main(
-        int argc,
-        const char *argv[]
-) {
-   int i = 0;
-   int j = 0;
-   int stop = 1;
-   OmlValueU v[4];
-   pid_t pid;
-   int mypipe[2];
-   char command[256]; 
-   char command2[256] = "256";
-   char macAddress[256] = "e";
-   //memset(command2, 0, 256*sizeof(char));
-   int* argc_;
-   const char** argv_;
-   char* ifwifi;
-//   char mem_used[64];
-   omlc_init(argv[ 0 ], &argc, argv, o_log);
-   argc_ = &argc;
-   argv_ = argv;
-   
-   ifwifi = "ath0";
-   if (pipe (mypipe))
-   {
-     fprintf (stderr, "Pipe failed.\n");
-     return EXIT_FAILURE;
-   }
-   pid = fork ();
-   if (pid == (pid_t) 0)
-   {
-     close (mypipe[0]);
-     close(1);
-     dup(mypipe[1]);
-     if (execl("/sbin/ifconfig", "ifconfig", "ath0", NULL) < 0){
+main(int argc, const char *argv[])
+{
+  int i = 0;
+  int j = 0;
+  int stop = 1;
+  OmlValueU v[4];
+  pid_t pid;
+  int mypipe[2];
+  char command[256];
+  char command2[256] = "256";
+  char macAddress[256] = "e";
+  int* argc_;
+  const char** argv_;
+  char* ifwifi;
+  omlc_init(argv[ 0 ], &argc, argv, o_log);
+  argc_ = &argc;
+  argv_ = argv;
+
+  ifwifi = "ath0";
+  if (pipe (mypipe)) {
+    fprintf (stderr, "Pipe failed.\n");
+    return EXIT_FAILURE;
+  }
+  pid = fork ();
+  if (pid == (pid_t) 0) {
+    close (mypipe[0]);
+    close(1);
+    dup(mypipe[1]);
+    if (execl("/sbin/ifconfig", "ifconfig", "ath0", NULL) < 0) {
+      printf("execl failed\n");
+      exit(1);
+    }
+  } else if (pid < (pid_t) 0) {
+    fprintf (stderr, "Fork failed.\n");
+    return EXIT_FAILURE;
+  } else {
+    close(mypipe[1]);
+    close(0);
+    dup(mypipe[0]);
+    scanf ("%s",command);
+    scanf ("%s",command);
+    scanf ("%s",command);
+    scanf ("%s",command);
+    scanf ("%s",command);
+    strcpy(macAddress, command);
+    close(mypipe[0]);
+    while(strcmp(command,command2) != 0) {
+      strcpy(command2, command);
+      scanf ("%s",command);
+    }
+  }
+
+  oml_mp = omlc_add_mp("wifi_info", oml_def);
+
+  omlc_start();
+  printf("mac address %s \n",macAddress);
+
+  while(1) {
+    i = 0;
+    stop = 1;
+    if (pipe (mypipe)) {
+      fprintf (stderr, "Pipe failed.\n");
+      return EXIT_FAILURE;
+    }
+    pid = fork ();
+    if (pid == (pid_t) 0) {
+      close (mypipe[0]);
+      close(1);
+      dup(mypipe[1]);
+      if (execl("/sbin/wlanconfig", "wlanconfig", "ath0", "list", NULL) < 0) {
         printf("execl failed\n");
         exit(1);
-     
-     }
-       
-        
-   }else if (pid < (pid_t) 0){
-     fprintf (stderr, "Fork failed.\n");
-     return EXIT_FAILURE;
-   }
-   else {
-     close(mypipe[1]);
-     close(0);
-     dup(mypipe[0]);
-     scanf ("%s",command);
-     scanf ("%s",command);
-     scanf ("%s",command);
-     scanf ("%s",command);
-     scanf ("%s",command);
-     strcpy(macAddress, command);
-     close(mypipe[0]);
-     while(strcmp(command,command2) != 0){
+      }
+    } else if (pid < (pid_t) 0) {
+      fprintf (stderr, "Fork failed.\n");
+      return EXIT_FAILURE;
+    } else {
+      close(mypipe[1]);
+      close(0);
+      dup(mypipe[0]);
+      scanf ("%s",command);
+
+      while(stop) {
         strcpy(command2, command);
-        scanf ("%s",command);
-     }
-     
-   }
-   
-      
-    oml_mp = omlc_add_mp("wifi_info", oml_def);
-    
-   
-    omlc_start();
-    printf("mac address %s \n",macAddress);
 
-   while(1){
-       i = 0;
-       stop = 1;
-       if (pipe (mypipe))
-         {
-           fprintf (stderr, "Pipe failed.\n");
-           return EXIT_FAILURE;
-         }
-       pid = fork ();
-       if (pid == (pid_t) 0)
-       {
-         close (mypipe[0]);
-         close(1);
-         dup(mypipe[1]);
-         if (execl("/sbin/wlanconfig", "wlanconfig", "ath0", "list", NULL) < 0){
-            printf("execl failed\n");
-            exit(1);
-        
-         }
-       
-        
-       }else if (pid < (pid_t) 0){
-         fprintf (stderr, "Fork failed.\n");
-         return EXIT_FAILURE;
-       }
-       else {
-         close(mypipe[1]);
-         close(0);
-         dup(mypipe[0]);
-         scanf ("%s",command);
-         
-         while(stop){
-           strcpy(command2, command);
-           
-           if(strcmp(command,"ADDR") != 0){
+        if(strcmp(command,"ADDR") != 0) {
+          omlc_set_const_string(v[0], command2);
+          scanf ("%s",command);
+          scanf ("%s",command);
+          scanf ("%s",command);
+          scanf ("%s",command);
 
-                omlc_set_const_string(v[0], command2); 
-                scanf ("%s",command);
+          v[1].longValue = atol(command);
+          scanf ("%s",command);
 
-                scanf ("%s",command);
+          v[2].longValue = atol(command);
+          omlc_set_const_string(v[3], macAddress);
 
-                scanf ("%s",command);
-
-                scanf ("%s",command);
-
-                v[1].longValue = atol(command);
-                scanf ("%s",command);
-
-                v[2].longValue = atol(command);
-                omlc_set_const_string(v[3], macAddress); 
-                
-                 omlc_process(oml_mp, v);
-                 //stop=0;
-                 for(j = 0; j < 9; j++){
-                   scanf ("%s",command);
-                   strcpy(command2, command);
-                 }
-                 while(command[0] != '0' && command[2] != ':'){  
-                 printf("result wlanconfig 11: %s \n ", command);
-                   scanf ("%s",command);
-                   if(strcmp(command,command2) == 0){
-                     stop = 0;
-                     break;
-                   }
-                   strcpy(command2, command);
-                 }
-                 
-                 
-                 //printf("result wlanconfig 2: %s \n ", command);
-                //printf("result wlanconfig: %s \n ", command);
-           }else{
-             for(i = 0; i < 16; i++){
-                
-                scanf ("%s",command);
-             }
-           }
-           //scanf ("%s",command);
-        
-         }
-       
+          omlc_process(oml_mp, v);
+          //stop=0;
+          for(j = 0; j < 9; j++) {
+            scanf ("%s",command);
+            strcpy(command2, command);
+          }
+          while(command[0] != '0' && command[2] != ':') {
+            printf("result wlanconfig 11: %s \n ", command);
+            scanf ("%s",command);
+            if(strcmp(command,command2) == 0) {
+              stop = 0;
+              break;
+            }
+            strcpy(command2, command);
+          }
+        } else {
+          for(i = 0; i < 16; i++) {
+            scanf ("%s",command);
+          }
         }
-        sleep(1);
-   }
-          
-//        v[0].longValue = (long)interface_stat.rx_bytes;
-//        v[1].longValue = (long)interface_stat.tx_bytes;
-//        v[2].stringPtrValue = (char*) mem_used;
-//        //printf("memory %s \n", mem_used);
-//        v[3].longValue = (long)cpu_info.user;
-//        v[4].longValue = (long)cpu_info.total;
-//        
-//        
-//        omlc_process(sigar_mp->mp, v);
+      }
+    }
+    sleep(1);
+  }
 
-       
-        
-        
-        
-    
-        
-        
-    
-    exit(0);    
+  return 0;
 }
