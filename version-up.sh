184eabec6cc23a6580ecd584b34feed859933298
Jolyon White <jolyon.white@nicta.com.au>
2010-09-10 18:43:40 +1000

    Make all of the applications separately buildable.
    - Top-level configure.ac and Makefile.am configure and build the apps
      recursively.
    - Each app has its own configure.ac so that it can be built and
      distributed independently.

---
+++ version-up.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ver=$1
+
+if [ "x$ver" == "x" ]; then
+	echo "Please set a version number"
+fi
+
+echo "Setting version number to ${ver} in..."
+
+files=$(find . -name configure.ac)
+
+for i in $files; do
+	echo $i
+    sed "s/\(AC_INIT(\[[^]]*\], *\)\[[0-9\.pre]*\]/\1[${ver}]/" $i > $i.new
+	mv $i.new $i
+done
+
