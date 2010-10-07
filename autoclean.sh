ee2e12ba6a479f91e6afe3375b950dba909505d0
Olivier Mehani <olivier.mehani@nicta.com.au>
2010-10-07 18:57:45 +1100

    Include ripwave-monitor in a full build.
    Add to *SUBDIRS in root configure.ac and Makefile.am, and cleaning
    script.
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>
    Signed-off-by: Jolyon White <jolyon.white@nicta.com.au>

--- autoclean.sh
+++ autoclean.sh
@@ -11,7 +11,7 @@ test -d autom4te.cache && ${RM} -fr autom4te.cache
 find . -name Makefile.in -exec ${RM} -f {} +
 ${RM} -f aclocal.m4 config.h.in config.guess config.sub configure depcomp install-sh ltmain.sh missing m4/*.m4
 
-APPS='gps iperf omf_nmetrics omf_trace otg wlanconfig'
+APPS='gps iperf omf_nmetrics omf_trace otg ripwave_monitor wlanconfig'
 
 for i in $APPS; do
 	$(cd $i; ./autoclean.sh)
