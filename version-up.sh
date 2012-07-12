48cb82868cbb57c08073e0847e0a754ceaf7ac07
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-07-12 17:19:33 +1000

    Update version number in app descriptions too
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- version-up.sh
+++ version-up.sh
@@ -1,17 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 ver=$1
 
 if [ "x$ver" == "x" ]; then
-	echo "Please set a version number"
+	echo "usage: $0 MAJOR.MINOR.REV" >&2
+	exit 1
 fi
 
-echo "Setting version number to ${ver} in..."
+echo -n "Setting version number to ${ver} in:"
 
-files=$(find . -name configure.ac)
+conffiles=$(find . -name configure.ac)
 
-for i in $files; do
-	echo $i
-	sed -i "s/\(AC_INIT(\[[^]]*\], *\)\[[0-9\.pre]*\]/\1[${ver}]/" $i
+for i in $conffiles; do
+	echo -n " $i"
+	sed -i "s/\(AC_INIT(\[[^]]*\], *\)\[\([0-9.]\++oml\)\?[0-9\.pre]*\]/\1[\2${ver}]/" $i
 done
 
+appdeffiles=$(find . -name *.rb.in)
+for i in $appdeffiles; do
+	echo -n " $i"
+	sed -i "s/a\.version([^)]*)/a.version(${ver//./, })/" $i
+done
+
+echo .
