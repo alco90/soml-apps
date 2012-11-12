990a77580424f29bfb4707cc726b2b2460fa4956
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-11-13 10:44:08 +1100

    Use git-version-gen in base package
    Quicky way:
     sed -i "s/AC_INIT(\[\([^]]\+\)\],.*)/AC_INIT([\1],\n\tm4_esyscmd([build-aux\/git-version-gen .tarball-version]),\n\t[oml-user@lists.nicta.com.au])/;s/AC_CONFIG_AUX_DIR(\[.\])/AC_CONFIG_MACRO_DIR([m4])\nAC_CONFIG_AUX_DIR([build-aux])/;s/AC_PROG_CC/&\ngl_EARLY/;s/AC_PROG_MAKE_SET/&\n\ngl_INIT/" configure.ac
     gnulib-tool --source-base gnulib --add-import git-version-gen
     vim Makefile.am
       (add -I m4 to ACLOCAL_AMFLAGS and m4/gnulib_cache.m4 to EXTRA_DIST)
     git add configure.ac Makefile.am m4/gnulib-cache.m4
    
    This might also require some fiddling with autogen.sh and autoclean.sh,
    as well as .gitignore.
    
    refs: #833
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+APPS=`sed -n "s/SUBDIRS * =//p" Makefile.am`
+for i in $APPS; do
+	cd $i
+	./autogen.sh --quick # Don't run autoreconf just yet
+	cd -
+done
+
+rm -rf autom4te.cache/
+gnulib-tool --update --quiet --quiet
+chmod a+x build-aux/git-version-gen
+autoreconf -i $*
