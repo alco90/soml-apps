454d867580a745e308112050671f399edffc1723
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-12-17 16:29:42 +1100

    [collectd] Code cleanup and reindent
    * Use --oml-collect and --oml-domain
    * Test return values
    * Log collectd-write-oml2 version when used
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- collectd/write_oml/write_oml.c
+++ collectd/write_oml/write_oml.c
@@ -42,12 +42,26 @@
 #include "utils_cache.h"
 #include "utils_parse_option.h"
 
+#ifdef HAVE_CONFIG_H
+/* Avoid inheritng collectd's definitions and/or warnings */
+# undef PACKAGE
+# undef PACKAGE_BUGREPORT
+# undef PACKAGE_NAME
+# undef PACKAGE_STRING
+# undef PACKAGE_TARNAME
+# undef PACKAGE_VERSION
+# undef VERSION
+# include "config.h"
+#else
+# define PACKAGE_STRING __FILE__
+#endif
+
 static const char *config_keys[] =
 {
-   "ServerURL",
-   "ContextName",
-   "NodeName",
-   "StartupDelay"
+  "NodeID",
+  "Domain",
+  "CollectURI",
+  "StartupDelay"
 };
 static int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
 
@@ -67,8 +81,8 @@ typedef struct _mpoint {
 } MPoint;
 
 typedef struct {
-  char* server_url;
-  char* context_name;
+  char* collect_uri;
+  char* domain;
   char* node_id;
   int   startup_delay;
 
@@ -84,7 +98,7 @@ static time_t start_time;
 static MPoint*
 find_mpoint_struct(
     const char* name
-) {
+    ) {
   MPoint* mp = session.mpoint;
 
   while (mp) {
@@ -101,12 +115,12 @@ configure_mpoint(
     MPoint* mp,
     const data_set_t *ds,
     const value_list_t *vl
-) {
+    ) {
 
-//  strncpy(mp->plugin, vl->plugin, sizeof(mp->plugin));
-//  strncpy(mp->plugin_instance, vl->plugin_instance, sizeof(mp->plugin_instance));
-//  strncpy(mp->type, vl->type, sizeof (mp->type));
-//  strncpy(mp->type_instance, vl->type_instance, sizeof (mp->type_instance));
+  //  strncpy(mp->plugin, vl->plugin, sizeof(mp->plugin));
+  //  strncpy(mp->plugin_instance, vl->plugin_instance, sizeof(mp->plugin_instance));
+  //  strncpy(mp->type, vl->type, sizeof (mp->type));
+  //  strncpy(mp->type_instance, vl->type_instance, sizeof (mp->type_instance));
 
   mp->mp_defs = (OmlMPDef*)malloc((ds->ds_num + 6 + 1) * sizeof(OmlMPDef));
 
@@ -127,21 +141,22 @@ configure_mpoint(
     md->name = s;
     assert(d->type <= 3);
     switch(d->type) {
-    //  see plugin.h
-    //    typedef unsigned long long counter_t;
-    //    typedef double gauge_t;
-    //    typedef int64_t derive_t;
-    //    typedef uint64_t absolute_t;
-    case 0: md->param_types = OML_INT64_VALUE; break;
-    case 1: md->param_types = OML_DOUBLE_VALUE; break;
-    case 2: md->param_types = OML_INT64_VALUE; break;
-    case 3: md->param_types = OML_UINT64_VALUE; break;
+      //  see plugin.h
+      //    typedef unsigned long long counter_t;
+      //    typedef double gauge_t;
+      //    typedef int64_t derive_t;
+      //    typedef uint64_t absolute_t;
+      case 0: md->param_types = OML_INT64_VALUE; break;
+      case 1: md->param_types = OML_DOUBLE_VALUE; break;
+      case 2: md->param_types = OML_INT64_VALUE; break;
+      case 3: md->param_types = OML_UINT64_VALUE; break;
     }
   }
   // NULL out last one
   OmlMPDef* md = &mp->mp_defs[ds->ds_num + offset];
   md->name = 0; md->param_types = (OmlValueT)0;
   mp->oml_mp = omlc_add_mp(mp->name, mp->mp_defs);
+  DEBUG("oml_writer plugin: New measurement point %s", mp->name);
 }
 
 static MPoint*
@@ -149,7 +164,7 @@ create_mpoint(
     const char* name,
     const data_set_t *ds,
     const value_list_t *vl
-) {
+    ) {
   MPoint* mp;
   // Create MPoint and insert it into session's existing MP chain.
   mp = (MPoint*)malloc(sizeof(MPoint));
@@ -166,7 +181,7 @@ find_mpoint(
     const char* name,
     const data_set_t *ds,
     const value_list_t *vl
-) {
+    ) {
   MPoint* mp = find_mpoint_struct(name);
 
   if (mp == NULL) {
@@ -176,22 +191,26 @@ find_mpoint(
     mp = create_mpoint(name, ds, vl);
   }
   if (! session.oml_intialized) {
-      // We are waiting for some time before we commit to a set of
-      // reportable measurements.
-      //
-      time_t now;
-      time(&now);
-      if ((now - start_time) < session.startup_delay) {
-        return NULL;  // let's wait a bit longer
-      }
-      // OK it's time to commit
-      pthread_mutex_lock(&session.init_lock);
-      if (! session.oml_intialized) { // just make sure nothing has changed since we aquired the lock
-        DEBUG("oml_writer plugin: Starting OML");
-        omlc_start();
+    // We are waiting for some time before we commit to a set of
+    // reportable measurements.
+    //
+    time_t now;
+    time(&now);
+    if ((now - start_time) < session.startup_delay) {
+      return NULL;  // let's wait a bit longer
+    }
+    // OK it's time to commit
+    pthread_mutex_lock(&session.init_lock);
+    if (! session.oml_intialized) { // just make sure nothing has changed since we aquired the lock
+      DEBUG("oml_writer plugin: Starting OML");
+      if(omlc_start()==0) {
         session.oml_intialized = 1;
+      } else {
+        ERROR("oml_writer plugin: Could not initialise OML");
+        mp = NULL;
       }
-      pthread_mutex_unlock(&session.init_lock);
+    }
+    pthread_mutex_unlock(&session.init_lock);
 
   }
   return mp;
@@ -202,7 +221,7 @@ oml_write (
     const data_set_t *ds,
     const value_list_t *vl,
     user_data_t __attribute__((unused)) *user_data
-) {
+    ) {
   MPoint* mp = find_mpoint(ds->type, ds, vl);
   if (mp == NULL || !session.oml_intialized) return(0);
 
@@ -216,6 +235,8 @@ oml_write (
     return(-1);
   }
 
+  DEBUG("oml_writer plugin: New data in %s (%s, %s, %s, %s, %s)",
+      mp->name, vl->host, vl->plugin, vl->plugin_instance, vl->type, vl->type_instance);
   omlc_set_int64(v[0], vl->time);
   omlc_set_string(v[1], vl->host != NULL ? (char*)vl->host : "");
   omlc_set_string(v[2], vl->plugin != NULL ? (char*)vl->plugin : "");
@@ -230,16 +251,16 @@ oml_write (
 
     assert(d->type <= 3);
     switch(d->type) {
-    //  see configure_mpoint()
-    case 0: omlc_set_int64(v[i + header], vi->counter); break;
-    case 1: omlc_set_double(v[i + header], vi->gauge); break;
-    case 2: omlc_set_int64(v[i + header], vi->derive); break;
-    case 3: omlc_set_uint64(v[i + header], vi->absolute); break;
+      //  see configure_mpoint()
+      case 0: omlc_set_int64(v[i + header], vi->counter); break;
+      case 1: omlc_set_double(v[i + header], vi->gauge); break;
+      case 2: omlc_set_int64(v[i + header], vi->derive); break;
+      case 3: omlc_set_uint64(v[i + header], vi->absolute); break;
     }
   }
   omlc_inject(mp->oml_mp, v);
-  for(i=1;i<6;i++)
-	  omlc_reset_string(v[i]);
+  for(i=1;i<header;i++)
+    omlc_reset_string(v[i]);
 
   return(0);
 }
@@ -248,16 +269,16 @@ static int
 oml_config(
     const char *key,
     const char *value
-) {
-  if (strcasecmp ("ServerURL", key) == 0) {
-    session.server_url = (char*)malloc(strlen(value) + 1);
-    strncpy(session.server_url, value, strlen(value));
-  } else if (strcasecmp ("ContextName", key) == 0) {
-    session.context_name = (char*)malloc(strlen(value) + 1);
-    strncpy(session.context_name, value, strlen(value));
-  } else if (strcasecmp ("NodeName", key) == 0) {
+    ) {
+  if (strcasecmp ("NodeID", key) == 0) {
     session.node_id = (char*)malloc(strlen(value) + 1);
     strncpy(session.node_id, value, strlen(value));
+  } else if (strcasecmp ("ContextName", key) == 0) {
+    session.domain = (char*)malloc(strlen(value) + 1);
+    strncpy(session.domain, value, strlen(value));
+  } else if (strcasecmp ("CollectURI", key) == 0) {
+    session.collect_uri = (char*)malloc(strlen(value) + 1);
+    strncpy(session.collect_uri, value, strlen(value));
   } else if (strcasecmp ("StartupDelay", key) == 0) {
     session.startup_delay = atoi(value);
   } else {
@@ -268,48 +289,48 @@ oml_config(
 
 static void o_log_collectd(int log_level, const char* format, ...)
 {
- va_list va;
- va_start(va, format);
- switch(log_level) {
- case O_LOG_ERROR:
-	 ERROR(format, va);
-	 break;
- case O_LOG_WARN:
-	 WARNING(format, va);
-	 break;
- case O_LOG_INFO:
-	 INFO(format, va);
-	 break;
- case O_LOG_DEBUG:
- case O_LOG_DEBUG2:
- case O_LOG_DEBUG3:
- case O_LOG_DEBUG4:
-	 DEBUG(format, va);
-	 break;
- default:
-	 NOTICE(format, va);
-	 break;
- }
- va_end(va);
+  va_list va;
+  va_start(va, format);
+  switch(log_level) {
+    case O_LOG_ERROR:
+      ERROR(format, va);
+      break;
+    case O_LOG_WARN:
+      WARNING(format, va);
+      break;
+    case O_LOG_INFO:
+      INFO(format, va);
+      break;
+    case O_LOG_DEBUG:
+    case O_LOG_DEBUG2:
+    case O_LOG_DEBUG3:
+    case O_LOG_DEBUG4:
+      DEBUG(format, va);
+      break;
+    default:
+      NOTICE(format, va);
+      break;
+  }
+  va_end(va);
 }
 
 static int
 oml_init(
     void
-) {
+    ) {
   const char* app_name = "collectd";
-  const char* argv[] = {"--oml-server", "file:-", "--oml-id", hostname_g, "--oml-exp-id", "collectd"};
+  const char* argv[] = {"--oml-id", hostname_g, "--oml-domain", "collectd", "--oml-collect", "file:-"};
   int argc = 6;
 
-  if (session.server_url != NULL) argv[1] = session.server_url;
-  if (session.node_id != NULL) argv[3] = session.node_id;
-  if (session.context_name != NULL) argv[5] = session.context_name;
-  omlc_init(app_name, &argc, argv, o_log_collectd);
+  NOTICE("oml_writer plugin: " PACKAGE_STRING);
 
-  return 0;
+  if (session.node_id != NULL) argv[1] = session.node_id;
+  if (session.domain != NULL) argv[3] = session.domain;
+  if (session.collect_uri != NULL) argv[5] = session.collect_uri;
+  return omlc_init(app_name, &argc, argv, o_log_collectd);
 }
 
-void
+  void
 module_register(void)
 
 {
@@ -322,3 +343,12 @@ module_register(void)
   plugin_register_init("write_oml", oml_init);
   plugin_register_write("write_oml", oml_write, /* user_data = */ NULL);
 } /* void module_register */
+
+/*
+ Local Variables:
+ mode: C
+ tab-width: 2
+ indent-tabs-mode: nil
+ End:
+ vim: sw=2:sts=2:expandtab
+ */
