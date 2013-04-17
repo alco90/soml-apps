2ebea8bade964ff8b2612aaf0e2cef14888f5edf
Olivier Mehani <olivier.mehani@nicta.com.au>
2013-04-17 11:49:06 +1000

    [nmetrics] Update coding style and header
    refs: #1100
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- nmetrics/nmetrics.c
+++ nmetrics/nmetrics.c
@@ -9,27 +9,14 @@
  * Author: Max Ott  <max.ott@nicta.com.au>, (C) 2009
  * Author: Olivier Mehani  <olivier.mehani@nicta.com.au>, (C) 2012
  *
- * Copyright (c) 2007-2012 National ICT Australia (NICTA)
+ * Copyright 2007-2013 National ICT Australia (NICTA)
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
-*/
-
+ * This software may be used and distributed solely under the terms of
+ * the MIT license (License).  You should find a copy of the License in
+ * COPYING or at http://opensource.org/licenses/MIT. By downloading or
+ * using this software you accept the terms and the liability disclaimer
+ * in the License.
+ */
 
 #include <config.h>
 #include <stdlib.h>
@@ -72,14 +59,9 @@ typedef struct _if_monitor_t {
   struct _if_monitor_t* next;
 } if_monitor_t;
 
-
-
 void
-run(
-  opts_t* opts,
-  oml_mps_t* oml_mps,
-  if_monitor_t* first_if
-) {
+run( opts_t* opts, oml_mps_t* oml_mps, if_monitor_t* first_if)
+{
   sigar_t* sigar_p;
   if_monitor_t* if_p;
   sigar_cpu_t c;
@@ -92,27 +74,27 @@ run(
 
       sigar_cpu_get(sigar_p, &c);
       oml_inject_cpu(oml_mps->cpu,
-		      c.user,
-		      (uint64_t) c.sys,
-		      (uint64_t)c.nice,
-		      (uint64_t)c.idle,
-		      (uint64_t)c.wait,
-		      (uint64_t)c.irq,
-		      (uint64_t)c.soft_irq,
-		      (uint64_t)c.stolen,
-		      (uint64_t)c.total);
+          c.user,
+          (uint64_t) c.sys,
+          (uint64_t)c.nice,
+          (uint64_t)c.idle,
+          (uint64_t)c.wait,
+          (uint64_t)c.irq,
+          (uint64_t)c.soft_irq,
+          (uint64_t)c.stolen,
+          (uint64_t)c.total);
     }
     if (opts->report_memory) {
 
       sigar_mem_get(sigar_p, &m);
 
       oml_inject_memory(oml_mps->memory,
-		      (uint64_t)m.ram,
-		      (uint64_t)m.total,
-		      (uint64_t)m.used,
-		      (uint64_t)m.free,
-		      (uint64_t)m.actual_used,
-		      (uint64_t)m.actual_free);
+          (uint64_t)m.ram,
+          (uint64_t)m.total,
+          (uint64_t)m.used,
+          (uint64_t)m.free,
+          (uint64_t)m.actual_used,
+          (uint64_t)m.actual_free);
     }
 
     if_p = first_if;
@@ -136,21 +118,21 @@ run(
         if_p->not_first = 1;
       }
       oml_inject_network(oml_mps->network,
-		      if_p->if_name,
-		      (uint64_t)(is.rx_packets - if_p->start_rx_packets),
-		      (uint64_t)(is.rx_bytes - if_p->start_rx_bytes),
-		      (uint64_t)(is.rx_errors - if_p->start_rx_errors),
-		      (uint64_t)(is.rx_dropped - if_p->start_rx_dropped),
-		      (uint64_t)(is.rx_overruns - if_p->start_rx_overruns),
-		      (uint64_t)(is.rx_frame - if_p->start_rx_frame),
-		      (uint64_t)(is.tx_packets - if_p->start_tx_packets),
-		      (uint64_t)(is.tx_bytes - if_p->start_tx_bytes),
-		      (uint64_t)(is.tx_errors - if_p->start_tx_errors),
-		      (uint64_t)(is.tx_dropped - if_p->start_tx_dropped),
-		      (uint64_t)(is.tx_overruns - if_p->start_tx_overruns),
-		      (uint64_t)(is.tx_collisions - if_p->start_tx_collisions),
-		      (uint64_t)(is.tx_carrier),
-		      (uint64_t)(is.speed / 1000000));
+          if_p->if_name,
+          (uint64_t)(is.rx_packets - if_p->start_rx_packets),
+          (uint64_t)(is.rx_bytes - if_p->start_rx_bytes),
+          (uint64_t)(is.rx_errors - if_p->start_rx_errors),
+          (uint64_t)(is.rx_dropped - if_p->start_rx_dropped),
+          (uint64_t)(is.rx_overruns - if_p->start_rx_overruns),
+          (uint64_t)(is.rx_frame - if_p->start_rx_frame),
+          (uint64_t)(is.tx_packets - if_p->start_tx_packets),
+          (uint64_t)(is.tx_bytes - if_p->start_tx_bytes),
+          (uint64_t)(is.tx_errors - if_p->start_tx_errors),
+          (uint64_t)(is.tx_dropped - if_p->start_tx_dropped),
+          (uint64_t)(is.tx_overruns - if_p->start_tx_overruns),
+          (uint64_t)(is.tx_collisions - if_p->start_tx_collisions),
+          (uint64_t)(is.tx_carrier),
+          (uint64_t)(is.speed / 1000000));
 
 
 
@@ -164,10 +146,8 @@ run(
 }
 
 int
-main(
-  int argc,
-  const char *argv[]
-) {
+main(int argc, const char *argv[])
+{
   char c;
   if_monitor_t* first = NULL;
   char *progname = strdup(argv[0]), *p=progname, *p2;
@@ -185,7 +165,7 @@ main(
   /* The canonical name is `nmetrics-oml2', so it clearly does not start with `om' */
   l = strlen(p);
   if (!strncmp(p, "om", MIN(l,2)) || !strncmp(p, "nmetrics_oml2", MIN(l,13))) {
-	  logwarn("Binary name `%s' is deprecated and will disappear soon, please use `nmetrics-oml2' instead\n", p);
+    logwarn("Binary name `%s' is deprecated and will disappear soon, please use `nmetrics-oml2' instead\n", p);
   }
   free(progname);
 
@@ -200,23 +180,32 @@ main(
   while ((c = poptGetNextOpt(optCon)) >= 0) {
     switch (c) {
     case 'i': {
-      if_monitor_t* im = (if_monitor_t *)malloc(sizeof(if_monitor_t));
-      memset(im, 0, sizeof(if_monitor_t));
-      strncpy(im->if_name, g_opts->if_name, 64);
-      im->next = first;
-      first = im;
+                if_monitor_t* im = (if_monitor_t *)malloc(sizeof(if_monitor_t));
+                memset(im, 0, sizeof(if_monitor_t));
+                strncpy(im->if_name, g_opts->if_name, 64);
+                im->next = first;
+                first = im;
 
-      loginfo("Monitoring interface %s\n", g_opts->if_name);
-      break;
-    }
+                loginfo("Monitoring interface %s\n", g_opts->if_name);
+                break;
+              }
     }
   }
 
   oml_register_mps();  // defined in xxx_oml.h
   omlc_start();
 
-// Do some work
+  // Do some work
   run(g_opts, g_oml_mps, first);
 
   return(0);
 }
+
+/*
+ Local Variables:
+ mode: C
+ tab-width: 2
+ indent-tabs-mode: nil
+ End:
+ vim: sw=2:sts=2:expandtab
+*/
